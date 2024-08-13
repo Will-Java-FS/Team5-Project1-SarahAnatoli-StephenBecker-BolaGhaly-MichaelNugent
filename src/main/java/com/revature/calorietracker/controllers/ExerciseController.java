fc039ce27c7d75b0237fb3ea38f43b9e829805c8
Sarah Anatoli <ahmad.sarah.333@gmail.com>
2024-08-13 16:03:03 -0400

    add exercises to exercise table

--- src/main/java/com/revature/calorietracker/controllers/ExerciseController.java
+++ src/main/java/com/revature/calorietracker/controllers/ExerciseController.java
@@ -1,8 +1,24 @@
 package com.revature.calorietracker.controllers;
 
+import com.revature.calorietracker.models.Exercise;
+import com.revature.calorietracker.service.ExerciseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/exercises")
 public class ExerciseController {
+    @Autowired
+    private ExerciseService exerciseService;
 
+    @PostMapping
+    public ResponseEntity<Exercise> createExercise(@RequestBody Exercise exercise) {
+        Exercise createdExercise = exerciseService.saveExercise(exercise);
+        return ResponseEntity.status(200).body(createdExercise);
+    }
 }
--- src/main/java/com/revature/calorietracker/service/ExerciseService.java
+++ src/main/java/com/revature/calorietracker/service/ExerciseService.java
@@ -1,10 +1,18 @@
 package com.revature.calorietracker.service;
 
+import com.revature.calorietracker.models.Exercise;
+import com.revature.calorietracker.repos.ExerciseRepo;
 import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @Transactional
 public class ExerciseService {
+    @Autowired
+    private ExerciseRepo exerciseRepo;
 
+    public Exercise saveExercise(Exercise exercise) {
+        return exerciseRepo.save(exercise);
+    }
 }
