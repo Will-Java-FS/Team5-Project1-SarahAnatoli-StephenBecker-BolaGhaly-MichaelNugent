e423a54602982a1bc96b08d87db740d5eac5a4e0
BolaGhaly <bolaghaly63@gmail.com>
2024-08-17 16:56:24 -0400

    Add updateUserPassword function to admin user service

--- src/main/java/com/revature/calorietracker/service/AdminUserService.java
+++ src/main/java/com/revature/calorietracker/service/AdminUserService.java
@@ -6,6 +6,7 @@ import com.revature.calorietracker.models.User;
 import com.revature.calorietracker.repos.AdminUserRepo;
 import lombok.AllArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -16,6 +17,7 @@ import java.util.Optional;
 @AllArgsConstructor
 public class AdminUserService {
     AdminUserRepo adminUserRepo;
+    PasswordEncoder passwordEncoder;
 
     @PreAuthorize("hasRole('ADMIN')")
     public List<UserDTO> getAllUserAccounts() {
@@ -33,5 +35,12 @@ public class AdminUserService {
     public void deleteUserAccount(Long id) {
         User userInDB = adminUserRepo.getUserAccountById(id);
         adminUserRepo.delete(userInDB);
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    public void updateUserPassword(Long id, String newPassword) {
+        User user = adminUserRepo.getUserAccountById(id);
+        user.setPassword(passwordEncoder.encode(newPassword));
+        adminUserRepo.save(user);
     }
 }
