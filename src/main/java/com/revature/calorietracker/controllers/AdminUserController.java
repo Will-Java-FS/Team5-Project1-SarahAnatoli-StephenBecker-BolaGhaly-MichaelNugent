4ace4619764e59f7e0da6f7c0aa0e5e1c71594f5
BolaGhaly <bolaghaly63@gmail.com>
2024-08-17 16:54:58 -0400

    Add updateUserPassword function to admin user controller

--- src/main/java/com/revature/calorietracker/controllers/AdminUserController.java
+++ src/main/java/com/revature/calorietracker/controllers/AdminUserController.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -21,13 +22,13 @@ public class AdminUserController {
     AdminUserService adminUserService;
     AdminUserRepo adminUserRepo;
 
-    // "Admins can view all user accounts"
+    // Admins can view all user accounts
     @GetMapping("/admin/users")
     public List<UserDTO> getAllUserAccounts() throws Exception {
         return adminUserService.getAllUserAccounts();
     }
 
-    // "Admins can delete user accounts"
+    // Admins can delete a user's account
     @DeleteMapping("/admin/user/{id}")
     public ResponseEntity<String> deleteUserAccount(@PathVariable Long id) {
         Optional<User> userInDB = Optional.ofNullable(adminUserRepo.getUserAccountById(id));
@@ -35,10 +36,27 @@ public class AdminUserController {
         if (userInDB.isEmpty())
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User does not exist in the database (incorrect user id)!");
         else if (!userInDB.get().getRole().equals(Role.USER))
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User cannot be deleted!");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User cannot be deleted! User's role is '" + userInDB.get().getRole() + "'.");
         else {
             adminUserService.deleteUserAccount(id);
             return ResponseEntity.status(HttpStatus.OK).body("User account was successfully deleted!");
         }
     }
+
+    // Admins can reset a user's password
+    @PostMapping(value = "/admin/user/{id}/resetPassword")
+    public ResponseEntity<String> updateUserPassword(@PathVariable Long id, @RequestBody Map<String, String> passwordMap) {
+        Optional<User> userInDB = Optional.ofNullable(adminUserRepo.getUserAccountById(id));
+
+        if (userInDB.isEmpty())
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User does not exist in the database (incorrect user id)!");
+        else if (!userInDB.get().getRole().equals(Role.USER))
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Password cannot be updated! User's role is '" + userInDB.get().getRole() + "'.");
+        else {
+            String newPassword = passwordMap.get("password");
+            adminUserService.updateUserPassword(id, newPassword);
+            return ResponseEntity.status(HttpStatus.OK).body("Password was successfully updated!");
+        }
+    }
+
 }
