f28c3df8af488e03ca867d8785331f9e1728b010
BolaGhaly <bolaghaly63@gmail.com>
2024-08-17 02:36:18 -0400

    Update admin user controller to include getAllUserAccounts function

--- src/main/java/com/revature/calorietracker/controllers/AdminUserController.java
+++ src/main/java/com/revature/calorietracker/controllers/AdminUserController.java
@@ -1,29 +1,22 @@
 package com.revature.calorietracker.controllers;
 
-import com.revature.calorietracker.models.User;
+import com.revature.calorietracker.dto.UserDTO;
 import com.revature.calorietracker.service.AdminUserService;
-import org.springframework.security.access.prepost.PreAuthorize;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-//ADMIN routes: "/admin/user/**"; as opposed to USER routes: "/user/**".
-@RequestMapping("/admin/user")
+@AllArgsConstructor
 public class AdminUserController {
+    @Autowired
     AdminUserService adminUserService;
-    @GetMapping("/{id}")
-    public User getById(@PathVariable int id){return null;}
-    //Get all users.  Need role authentication.
-    @GetMapping("/all")
-    public List<User> getAll(){return adminUserService.getAll();}
-    @PatchMapping("/{id}/role")
-    public User updateRoleById(@PathVariable long id){
-        return adminUserService.updateRoleById(id);
-    }
-    //Create User
-    //Delete User
-}
 
-//    @PreAuthorize("#id==authentication.principal.id or hasRole('ROLE_ADMIN')") //USER privileges means requester must own data.  However, ADMIN requester can always access.
-//@PreAuthorize("#id==authentication.principal.id or hasRole('ROLE_ADMIN')") //USER privileges means requester must own data.  However, ADMIN requester can always access.
+    // "Admins can view all user accounts"
+    @GetMapping("/admin/users")
+    public List<UserDTO> getAllUserAccounts() throws Exception {
+        return adminUserService.getAllUserAccounts();
+    }
+}
