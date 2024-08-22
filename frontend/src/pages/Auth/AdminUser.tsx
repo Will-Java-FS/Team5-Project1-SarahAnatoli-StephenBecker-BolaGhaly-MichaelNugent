4da77d80ec0bf92f2c87ff92ad38f6a444df6dbd
BolaGhaly <bolaghaly63@gmail.com>
2024-08-22 14:41:24 -0400

    Add password input field to each user

--- frontend/src/pages/Auth/AdminUser.tsx
+++ frontend/src/pages/Auth/AdminUser.tsx
@@ -9,6 +9,7 @@ export const AdminUser = () => {
   const [editMode, setEditMode] = useState(false);
   const [editUserId, setEditUserId] = useState<number>();
   const usernameRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
   const emailRef = useRef<HTMLInputElement>(null);
   const firstNameRef = useRef<HTMLInputElement>(null);
   const lastNameRef = useRef<HTMLInputElement>(null);
@@ -62,7 +63,7 @@ export const AdminUser = () => {
   const saveChangesForUserInfo = async (id: number) => {
     try {
       setEditMode(false);
-      const response = await axiosInstance.patch(`/admin/user/${id}`, {
+      const response1 = await axiosInstance.patch(`/admin/user/${id}`, {
         username: usernameRef.current!.value,
         email: emailRef.current!.value,
         firstName: firstNameRef.current!.value,
@@ -74,9 +75,19 @@ export const AdminUser = () => {
         dailyCalorieGoal: dailyCalorieGoalRef.current!.value,
       });
 
-      if (response.status === 200) {
-        getAllUsers();
-      }
+      console.log(passwordRef);
+
+      // const response2 = await axiosInstance.post(
+      //   `/admin/user/${id}/resetPassword`,
+      //   {
+      //     password: passwordRef.current!.value,
+      //   }
+      // );
+
+      //console.log(response2);
+      //&& response2.status === 200
+
+      if (response1.status === 200) getAllUsers();
     } catch (error) {
       console.error("Failed to update this user's information.", error);
     }
@@ -115,6 +126,24 @@ export const AdminUser = () => {
                   />
                 ) : (
                   <p>{user.email}</p>
+                )}
+              </div>
+              <div className="admin-user-info user-password-container">
+                <h6>Password</h6>
+                {editMode && editUserId === user.id ? (
+                  <input
+                    type="text"
+                    defaultValue={user.password}
+                    ref={passwordRef}
+                    onChange={(e) => {
+                      console.log(e.target.value);
+                      console.log(passwordRef.current?.value);
+                    }}
+                  />
+                ) : (
+                  <p className="user-password">
+                    {user.password.replace(/./g, "*")}
+                  </p>
                 )}
               </div>
               <div className="admin-user-info">
